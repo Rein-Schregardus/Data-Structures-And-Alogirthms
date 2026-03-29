f2765243a819a25fac6e7a0af84e15fba0c6d016
Rein-Schregardus <rein@schre.nl>
2026-03-29 11:57:00 +0200

    k-means finished + accuracy for compression

--- week-5/k_means_methods.py
+++ week-5/k_means_methods.py
@@ -9,23 +9,21 @@ def order_by_number(dataset_images, dataset_labels):
     Seperate the mnist dataset into a dictionary ordered by label
     """
     ordered_images = {0: [], 1: [], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[]}
-    print(range(len(dataset_images)))
-    for i, _ in range(len(dataset_images)):
+    for i, _ in enumerate(dataset_images):
         ordered_images[dataset_labels[i]].append(dataset_images[i])
-    print(ordered_images)
     return ordered_images
 
 
-def normalize_and_vectorize_image(image):
+def normalize_and_vectorize_image(image: np.array):
     """
     Turn a image of variable dimesions (must be square) into a vector and normalize values between 0 and 1
     """
     flat = image.flatten()
 
     maximum = np.max(flat)
-    normalised = flat / maximum
+    normalised: np.array = flat / maximum
 
-    return normalised
+    return normalised.astype(np.half)
 
 
 
@@ -38,14 +36,12 @@ def dist(a: np.array, b: np.array):
     return dist
 
 
-
-
-def k_means(datapoints: tuple[np.array], clusters: int) -> set[tuple]:
+def k_means(datapoints: tuple[np.array], clusters_amount: int) -> set[tuple]:
     """
     Find the cluster centers
     """
-    clusterOrigins: np.array[np.array] = np.array(choices(datapoints, k=clusters))
-    new_origins: np.array[np.array]
+    clusterOrigins: np.array[np.array] = np.array(choices(datapoints, k=clusters_amount), dtype=np.half)
+    new_origins = list()
     while True:
 
         clusters = dict()
@@ -53,21 +49,24 @@ def k_means(datapoints: tuple[np.array], clusters: int) -> set[tuple]:
         # reassign points to cluster_origins
         for point in datapoints:
             closest_cluster = min(clusterOrigins, key=lambda c : dist(c, point))
-            if (closest_cluster not in list(clusters.keys())):
+
+            closest_cluster = closest_cluster.tobytes()
+            if (closest_cluster not in clusters.keys()):
                 clusters[closest_cluster] = list()
 
             clusters[closest_cluster].append(point)
 
         # recalcuate origins
-        new_origins = set()
+        new_origins.clear()
         for key in clusters:
-            mean = np.mean(clusters[key])
-            new_origins.add((mean))
+            mean = np.mean(clusters[key], axis=0)
+            new_origins.append(mean)
 
         # base case
-        if np.array_equal(new_origins, clusterOrigins):
+        if np.array_equal(np.array(new_origins, dtype=np.half), clusterOrigins):
             return new_origins
         clusterOrigins = new_origins
+
 
 def vector_to_image(vector: np.array):
     """
@@ -86,3 +85,40 @@ def show_image(image):
     """
     plt.imshow(image, cmap="grey")
     plt.show()
+
+def make_prototype(train_images, train_labels, prototypes_per_number: int):
+    ordered_images = order_by_number(train_images, train_labels)
+    prototype: dict[int: np.array] = dict()
+    for num in ordered_images:
+        prototype[num] = k_means(ordered_images[num], prototypes_per_number)
+        print(f"finished for number {num}")
+    print("finished computing prototypes")
+    return prototype
+
+def get_distance(a: np.array, b: np.array):
+    return dist(a, b)
+
+def predict(image, prototypes):
+    img_vector = normalize_and_vectorize_image(image)
+    likeness  = {0: 999999999, 1: 999999999, 2: 999999999, 3: 999999999, 4: 999999999, 5: 999999999, 6: 999999999, 7: 999999999, 8: 999999999, 9: 999999999}
+    for number in prototypes:
+        prototypes_values = prototypes[number]
+        for prototype in prototypes_values:
+            differance = dist(prototype, img_vector)
+            if likeness[number] > differance:
+                likeness[number] = differance
+    return min(likeness, key=lambda k: likeness[k])
+
+def determine_accuracy(prototypes, images, labels):
+    total = len(images)
+    misses = 0
+    miss_for_num: dict  = {0: 0, 1: 0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+    for i, image in enumerate(images):
+        prediction = predict(image, prototypes)
+        # print(f"pred: {prediction} | act: {labels[i]}")
+        if prediction != labels[i]:
+            misses += 1
+            miss_for_num[labels[i]] += 1
+    print(f"misses {misses}")
+    print(f"accuracy {(total - misses) / total * 100} %")
+    print(miss_for_num)
